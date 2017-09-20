1524932645fa8b550348099dea8debf39e911fa8
kei-p <kei-p@sonicgarden.jp>
2017-09-20 14:51:13 +0900

    リファクタ

--- app/models/remotty_rails/participation.rb
+++ app/models/remotty_rails/participation.rb
@@ -1,6 +1,6 @@
 module RemottyRails
   class Participation < ActiveRecord::Base
     belongs_to :room, foreign_key: :remotty_rails_room_id
-    belongs_to :user,foreign_key: :remotty_rails_user_id
+    belongs_to :user, foreign_key: :remotty_rails_user_id
   end
 end
